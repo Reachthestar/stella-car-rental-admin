195df93e3c2589cc819e76d40d17a0ed7af9ca46
Jade-Rung-GH <jaitnipithe@hotmail.com>
2024-06-24 15:59:23 +0700

    Feature Booking Template Done, Hard Code

--- src/layouts/TopNav.jsx
+++ src/layouts/TopNav.jsx
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function TopNav() {
   return (
-    <div className="fixed w-full top-0 left-[260px] z-999 h-[70px] bg-top-nav shadow-md">
-      <div className="flex justify-between items-center px-7 border border-red-500">
+    <div className="fixed w-full top-0 left-[260px] z-999 h-[60px] bg-top-nav shadow-md">
+      <div className="flex justify-between items-center px-7 border border-blue-500">
         <div className="">
           <input />
         </div>
