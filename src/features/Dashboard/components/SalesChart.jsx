eadcecb1067b02d05522f8c3a9b2d8840ffbb68d
devaoun <devaoun147@gmail.com>
2024-06-26 17:55:56 +0700

    saleChart

--- src/features/Dashboard/components/SalesChart.jsx
+++ src/features/Dashboard/components/SalesChart.jsx
@@ -19,47 +19,47 @@ ChartJS.register(
 );
 import { faker } from '@faker-js/faker';
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Monthly Sales',
-    },
-  },
-};
-
-const labels = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Monthly Sales',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 100000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.8)',
-      borderRadius: '5',
-    },
-  ],
-};
-
 export default function SalesChart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Monthly Sales',
+      },
+    },
+  };
+  
+  const labels = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Monthly Sales',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100000 })),
+        backgroundColor: 'rgba(53, 162, 235, 0.8)',
+        borderRadius: '5',
+      },
+    ],
+  };
+  
   return (
     <div className="p-4 bg-white rounded-md shadow-md">
       <div style={{ width: '600px', height: '300px' }}>
