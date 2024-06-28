d2b91ad4b0ae808547f4e1223568307d2f276250
Reachthestar <nystar.aus@gmail.com>
2024-06-28 13:42:04 +0700

    created statics page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,46 +1,48 @@
-
-
 import Login from '../pages/Login';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import Bookings from "../pages/Bookings";
-import Settings from "../pages/Settings";
-import MainContainer from "../layouts/MainContainer";
-import Payments from "../pages/Payments";
-import Cars from "../pages/Cars";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Bookings from '../pages/Bookings';
+import Settings from '../pages/Settings';
+import MainContainer from '../layouts/MainContainer';
+import Payments from '../pages/Payments';
+import Cars from '../pages/Cars';
 import BookingContextProvider from '../contexts/booking-context';
 import CarsContextProvider from '../contexts/car-context';
-
+import Statistics from '../pages/Statistics';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainContainer />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Dashboard />,
       },
       {
-        path: "/bookings",
-        element:<Bookings />,
+        path: '/bookings',
+        element: <Bookings />,
       },
       {
-        path: "/cars",
-        element:<Cars />,
+        path: '/cars',
+        element: <Cars />,
       },
       {
-        path: "/payments",
+        path: '/payments',
         element: <Payments />,
       },
       {
-        path: "/settings",
+        path: '/statistics',
+        element: <Statistics />,
+      },
+      {
+        path: '/settings',
         element: <Settings />,
       },
     ],
   },
-  { path: '/login', element: <Login /> }
+  { path: '/login', element: <Login /> },
 ]);
 
 export default function Router() {
