589b307fe722e8b1fa139f7d713cfd1044978c2c
Jade-Rung-GH <jaitnipithe@hotmail.com>
2024-07-01 15:03:47 +0700

    paginate with filter and sort done

--- src/features/Dashboard/components/CarItem.jsx
+++ src/features/Dashboard/components/CarItem.jsx
@@ -1,10 +1,12 @@
-import carImage from '../../../assets/images/mini-car-01.png'
-
 export default function CarItem({ car }) {
   return (
     <div className="w-[100%] h-[200px] bg-white p-4 rounded-md shadow-m flex flex-col items-center shadow-md ">
       <h1>{`${car.brand} ${car.model} (${car.color})`}</h1>
-      <img className='w-full h-full min-h-0 object-contain' src={car.CarImages[0].imageUrl} alt={car.carName} />
+      <img
+        className="w-full h-full min-h-0 object-contain"
+        src={car.CarImages[0].imageUrl}
+        alt={car.carName}
+      />
     </div>
   );
 }
